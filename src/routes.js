c8eb6efa33ac4ba593c559f0d1a17a35fd53590e
Caio Robert <caio.robert@live.com>
2020-02-26 21:23:28 -0300

    chore: Cadastro de usuários

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Caio Robert',
-    email: 'caio.robert@live.com',
-    password_hash: '21465813531',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
